74ebfd9193d0628223f1a6b547e2fb5862435748
Akash kumar <158338367+akash-mehta47@users.noreply.github.com>
2025-12-17 23:38:24 +0530

    Time: 0 ms (100%), Space: 9 MB (20.29%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -2,19 +2,16 @@ class Solution {
 public:
     bool isValid(string s) {
         stack<char> st;
-        map<char,char> mpp={
-            {')','('},
-            {'}','{'},
-            {']','['}
-        };
-        for(auto c:s){
-            if(mpp.find(c)==mpp.end()){
-                st.push(c);
-            } else if(!st.empty() && mpp[c]==st.top()){
-                st.pop();
-            }
-            else{
-                return false;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='('||s[i]=='{'||s[i]=='['){
+                st.push(s[i]);
+            }else{
+                if(st.size()==0){
+                    return false;
+                }
+                if(st.top()=='(' && s[i]==')' || st.top()=='{' && s[i]=='}' || st.top()=='[' && s[i]==']' ){
+                    st.pop();
+                }else return false;
             }
         }
         return st.empty();
