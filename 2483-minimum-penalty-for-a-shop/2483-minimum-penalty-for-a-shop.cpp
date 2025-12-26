a1d8d21268ea536513a55c2a40cebb5b41a94ede
Akash kumar <158338367+akash-mehta47@users.noreply.github.com>
2025-12-26 22:55:08 +0530

    Time: 10 ms (42.62%), Space: 13.2 MB (98.36%) - LeetHub

--- 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.cpp
+++ 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.cpp
@@ -1,29 +1,20 @@
 class Solution {
 public:
-    int bestClosingTime(string customers) {
-        
-        int current_penalty = 0;
-        for (char c : customers) {
-            if (c == 'Y') {
-                current_penalty++;
+    int bestClosingTime(string s) {
+        int pen = 0;
+        for (char c : s) if (c == 'Y') pen++;
+
+        int best = pen, ans = 0;
+
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == 'Y') pen--;
+            else pen++;
+
+            if (pen < best) {
+                best = pen;
+                ans = i + 1;
             }
         }
-        
-        int min_penalty = current_penalty;
-        int best_hour = 0;
-        
-        for (int i = 0; i < customers.size(); i++) {
-            if (customers[i] == 'Y') {
-                current_penalty--;
-            } else {
-                current_penalty++;
-            }
-            if (current_penalty < min_penalty) {
-                min_penalty = current_penalty;
-                best_hour = i + 1;
-            }
-        }
-        
-        return best_hour;
+        return ans;
     }
 };
