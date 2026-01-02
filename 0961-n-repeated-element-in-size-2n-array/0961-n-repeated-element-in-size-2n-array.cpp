88d18f59de733be4c269d203380e496805115112
Akash kumar <158338367+akash-mehta47@users.noreply.github.com>
2026-01-03 01:42:04 +0530

    Time: 0 ms (100%), Space: 28.5 MB (53.81%) - LeetHub

--- 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.cpp
+++ 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.cpp
@@ -4,12 +4,7 @@ public:
         unordered_map<int,int> hash;
         for(int i=0;i<nums.size();i++){
             hash[nums[i]]++;
-        }
-        int temp=hash[0];
-        for(int i=0;i<hash.size();i++){
-            if(hash[nums[i]]>1){
-                return nums[i];
-            }
+            if(hash[nums[i]]>1) return nums[i];
         }
         return -1;
     }
