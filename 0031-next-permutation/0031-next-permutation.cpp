6fa17e25e389f2501f1dcf7ddbb0ea70c6b8fd7e
Akash kumar <158338367+akash-mehta47@users.noreply.github.com>
2026-02-19 00:17:28 +0530

    Time: 0 ms (100%), Space: 15.7 MB (32.6%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,6 +1,24 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        next_permutation(nums.begin(),nums.end());
+        int break_index=-1;
+        for(int i=nums.size()-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
+                break_index=i;
+                break;
+            }
+        }
+        if(break_index==-1){
+            reverse(nums.begin(),nums.end());
+            return;
+        }
+        for(int i=nums.size()-1;i>break_index;i--){
+            if(nums[i]>nums[break_index]){
+                swap(nums[i],nums[break_index]);
+                break;
+            }
+        }
+        reverse(nums.begin()+break_index+1,nums.end());
+        
     }
 };
