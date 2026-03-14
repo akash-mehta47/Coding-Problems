430b361f168fd673e3fff971d558c8b960200874
Akash kumar <158338367+akash-mehta47@users.noreply.github.com>
2026-03-14 23:28:05 +0530

    Time: 4 ms (73.66%), Space: 8 MB (78.63%) - LeetHub

--- 1362-closest-divisors/1362-closest-divisors.cpp
+++ 1362-closest-divisors/1362-closest-divisors.cpp
@@ -1,22 +1,31 @@
 class Solution {
 public:
+    vector<int> closestDivisors(int num) {
+        int num1=num+1;
+        int num2=num+2;
+        int diff=INT_MAX;
 
-    vector<int> findClosest(int target){
-        for(int i=sqrt(target);i>0;i--){
-            if(target%i==0){
-                return {i,target/i};
+        vector<int> ans(2);
+
+        for(int i=sqrt(num1);i>0;i--){
+            if(num1%i==0){
+                diff=abs(num1/i-i);
+                ans[0]=i;
+                ans[1]=num1/i;
+                break;
             }
         }
-        return {};
-    }
-    vector<int> closestDivisors(int num) {
-        vector<int> pair1= findClosest(num+1);
-        vector<int> pair2= findClosest(num+2);
 
-        if(abs(pair1[0]-pair1[1])<abs(pair2[0]-pair2[1])){
-            return pair1;
-        }else{
-            return pair2;
+        for(int i=sqrt(num2);i>0;i--){
+            if(num2%i==0){
+                if(abs(num2/i-i)<diff){
+                    ans[0]=i;
+                    ans[1]=num2/i;
+                }
+                break;
+            }
         }
+        return ans;
+
     }
 };
