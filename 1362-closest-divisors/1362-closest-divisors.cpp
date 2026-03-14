39514d7fb0af1ad173ad9bf0d0f59fb54e8f0084
Akash kumar <158338367+akash-mehta47@users.noreply.github.com>
2026-03-14 23:15:39 +0530

    Time: 7 ms (48.85%), Space: 8.2 MB (50.76%) - LeetHub

--- 1362-closest-divisors/1362-closest-divisors.cpp
+++ 1362-closest-divisors/1362-closest-divisors.cpp
@@ -1,41 +1,22 @@
 class Solution {
 public:
+
+    vector<int> findClosest(int target){
+        for(int i=sqrt(target);i>0;i--){
+            if(target%i==0){
+                return {i,target/i};
+            }
+        }
+        return {};
+    }
     vector<int> closestDivisors(int num) {
-        int first=num+1;
-        int second=num+2;
+        vector<int> pair1= findClosest(num+1);
+        vector<int> pair2= findClosest(num+2);
 
-        int minn1=INT_MAX;
-        int minn2=INT_MAX;
-
-        pair<int,int> pair1;
-        pair<int,int> pair2;
-
-        for(int i=1;i*i<=first;i++){
-            if(first%i==0){
-                int temp1=i;
-                int temp2=first/i;
-                if(minn1>abs(temp1-temp2)){
-                    minn1=abs(temp1-temp2);
-                    pair1.first=temp1;
-                    pair1.second=temp2;
-                }
-            }
-        }
-        for(int i=1;i*i<=second;i++){
-            if(second%i==0){
-                int temp1=i;
-                int temp2=second/i;
-                if(minn2>abs(temp1-temp2)){
-                    minn2=abs(temp1-temp2);
-                    pair2.first=temp1;
-                    pair2.second=temp2;
-                }
-            }
-        }
-        if(minn1<minn2){
-            return {pair1.first,pair1.second};
+        if(abs(pair1[0]-pair1[1])<abs(pair2[0]-pair2[1])){
+            return pair1;
         }else{
-            return {pair2.first,pair2.second};
+            return pair2;
         }
     }
 };
