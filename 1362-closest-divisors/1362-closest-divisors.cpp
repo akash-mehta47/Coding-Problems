81710f99f4d880df028a33c6bde2555b9f948219
Akash kumar <158338367+akash-mehta47@users.noreply.github.com>
2026-03-14 23:29:00 +0530

    Time: 6 ms (57.25%), Space: 8 MB (78.63%) - LeetHub

--- 1362-closest-divisors/1362-closest-divisors.cpp
+++ 1362-closest-divisors/1362-closest-divisors.cpp
@@ -5,22 +5,20 @@ public:
         int num2=num+2;
         int diff=INT_MAX;
 
-        vector<int> ans(2);
+        vector<int> ans;
 
         for(int i=sqrt(num1);i>0;i--){
             if(num1%i==0){
-                diff=abs(num1/i-i);
-                ans[0]=i;
-                ans[1]=num1/i;
+                diff=num1/i-i;
+                ans={i,num1/i};
                 break;
             }
         }
 
         for(int i=sqrt(num2);i>0;i--){
             if(num2%i==0){
-                if(abs(num2/i-i)<diff){
-                    ans[0]=i;
-                    ans[1]=num2/i;
+                if((num2/i-i)<diff){
+                    return {i,num2/i};
                 }
                 break;
             }
