f386f7c6348eda6d9dda3b2754bbd3118ead0197
Akash kumar <158338367+akash-mehta47@users.noreply.github.com>
2026-03-21 15:05:55 +0530

    Time: 0 ms (100%), Space: 36.9 MB (16.21%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.cpp
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -2,22 +2,17 @@ class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
         int n=nums.size();
-        int ans=-1;
-        unordered_map<int,long long> mpp;
-        long long sum=0;
+        vector<long long> pre(n+1);
         for(int i=0;i<n;i++){
-            sum+=nums[i];
-            mpp[i]=sum;
+            pre[i+1]=pre[i]+nums[i];
         }
-        for(int i=0;i<mpp.size();i++){
-            int left=0;
-            if(i>0) left = mpp[i-1];
-            int right = mpp[n-1]-mpp[i];
+        for(int i=0;i<n;i++){
+            long long left=pre[i];
+            long long right = pre[n]-pre[i+1];
             if(left==right){
-                ans=i;
-                return ans;
+                return i;
             }
         }
-        return ans;
+        return -1;        
     }
 };
