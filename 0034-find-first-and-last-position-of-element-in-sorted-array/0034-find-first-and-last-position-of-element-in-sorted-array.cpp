f3956d56073d6479aeb2600c32c6bfcb4927bcd5
Akash kumar <158338367+akash-mehta47@users.noreply.github.com>
2026-03-21 20:49:22 +0530

    Time: 0 ms (100%), Space: 17.6 MB (19.35%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,39 +1,42 @@
 class Solution {
 public:
 
-    int lowerBound(vector<int>& nums,int n,int target){
-        int low=0,high=n-1,ans=n;
+    int firstOcc(vector<int>& nums,int n,int target){
+        int low=0,high=n-1,first=-1;
         while(low<=high){
             int mid=low+(high-low)/2;
-            if(nums[mid]>=target){
-                ans=mid;
+            if(nums[mid]==target){
+                first=mid;
                 high=mid-1;
-            }else{
+            }
+            if(nums[mid]<target){
                 low=mid+1;
+            }else{
+                high=mid-1;
             }
         }
-        return ans;
+        return first;
     }
-    int upperBound(vector<int>& nums,int n,int target){
-        int low=0,high=n-1,ans=n;
+    int secondOcc(vector<int>& nums,int n,int target){
+        int low=0,high=n-1,last=-1;
         while(low<=high){
             int mid=low+(high-low)/2;
+            if(nums[mid]==target){
+                last=mid;
+                low=mid+1;
+            }
             if(nums[mid]>target){
-                ans=mid;
                 high=mid-1;
             }else{
                 low=mid+1;
             }
         }
-        return ans;
+        return last;
     }
     vector<int> searchRange(vector<int>& nums, int target) {
-        int n=nums.size();
-        int lb = lowerBound(nums,n,target);
-        if(lb==nums.size() || nums[lb]!=target){
-            return {-1,-1};
-        }
-        int ub = upperBound(nums,n,target);
-        return {lb,ub-1};
+        int first=firstOcc(nums,nums.size(),target);
+        if(first==-1) return {-1,-1};
+        int second=secondOcc(nums,nums.size(),target);
+        return {first,second};
     }
 };
