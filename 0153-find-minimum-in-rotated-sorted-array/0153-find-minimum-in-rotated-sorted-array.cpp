ba6d8c002b86584a76c29c685842b1cd24711453
Akash kumar <158338367+akash-mehta47@users.noreply.github.com>
2026-03-23 08:37:16 +0530

    Time: 0 ms (100%), Space: 14.1 MB (79.05%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
@@ -5,13 +5,12 @@ public:
         int ans=INT_MAX;
         while(low<=high){
             int mid=low+(high-low)/2;
-            ans=min(ans,nums[mid]);
             if(nums[mid]>nums[high]){
+                ans=min(ans,nums[low]);
                 low=mid+1;
-                ans=min(ans,nums[mid]);
             }else{
-                high=mid-1;
                 ans=min(ans,nums[mid]);
+                high=mid-1;
             }
         }
         return ans;
