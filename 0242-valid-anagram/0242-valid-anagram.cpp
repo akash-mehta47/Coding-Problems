12f6ae77e8d4cecb1a4d8113c3c89c145bac8df7
Akash kumar <158338367+akash-mehta47@users.noreply.github.com>
2026-04-14 18:25:18 +0530

    Time: 3 ms (46.39%), Space: 9.9 MB (10.42%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size() != t.size()) return false;
+        unordered_map<int,int> hash;
+        for(int i=0;i<s.size();i++){
+            hash[s[i]]++;
+            hash[t[i]]--;
+        }
+        for(auto it:hash){
+            if(it.second != 0){
+                return false;
+            }
+        }
+        return true;
+    }
+};
