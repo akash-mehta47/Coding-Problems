6ea50a61a2219d8a791e2bbbed25711b7fd24a8a
Akash kumar <158338367+akash-mehta47@users.noreply.github.com>
2026-05-14 23:36:44 +0530

    Time: 0 ms (100%), Space: 121.9 MB (43.84%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -10,16 +10,6 @@
  */
 class Solution {
 public:
-    ListNode* findMiddle(ListNode* head){
-        ListNode* slow=head;
-        ListNode* fast=head;
-
-        while(fast->next != NULL && fast->next->next != NULL){
-            slow=slow->next;
-            fast=fast->next->next;
-        }
-        return slow;
-    }
 
     ListNode* reverseLL(ListNode* head){
         ListNode* prev = NULL;
@@ -35,16 +25,27 @@ public:
 
     bool isPalindrome(ListNode* head) {
         if(head==NULL || head->next==NULL) return true;
-        ListNode* temp=findMiddle(head);
+
+        ListNode* slow=head;
+        ListNode* fast=head;
+
+        while(fast->next != NULL && fast->next->next != NULL){
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+
+        ListNode* newHead=reverseLL(slow->next);
         ListNode* first=head;
-        ListNode* second=reverseLL(temp->next);
+        ListNode* second=newHead;
         while(second!=NULL){
             if(first->val!=second->val){
+                reverseLL(newHead);
                 return false;
             }
             first=first->next;
             second=second->next;
         }
+        reverseLL(newHead);
         return true;
     }
 };
