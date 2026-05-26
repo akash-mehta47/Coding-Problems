688d61ade2e252176c67f4d952644f9d3cb846e3
Akash kumar <158338367+akash-mehta47@users.noreply.github.com>
2026-05-26 22:37:55 +0530

    Time: 0 ms (100%), Space: 9.5 MB (35.03%) - LeetHub

---
+++ 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int numberOfSpecialChars(string word) {
+        vector<int> freq(128,0);
+        for(char ch:word){
+            freq[ch-'A']++;
+        }
+        int count=0;
+        for(int i=0;i<26;i++){
+            if(freq[i]>0 && freq[i+32]>0){
+                count++;
+            }
+        }
+        return count;
+    }
+};
