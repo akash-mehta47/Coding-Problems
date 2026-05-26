09818bcdc3e7fc8ab45936b52ca5e818c449bb12
Akash kumar <158338367+akash-mehta47@users.noreply.github.com>
2026-05-26 22:38:37 +0530

    Time: 0 ms (100%), Space: 9.4 MB (35.03%) - LeetHub

--- 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.cpp
+++ 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.cpp
@@ -7,7 +7,7 @@ public:
         }
         int count=0;
         for(int i=0;i<26;i++){
-            if(freq[i]>0 && freq[i+32]>0){
+            if(freq[i]>0 && freq[i+'a'-'A']>0){
                 count++;
             }
         }
