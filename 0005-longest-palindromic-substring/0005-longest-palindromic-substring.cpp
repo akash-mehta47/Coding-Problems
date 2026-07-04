3ae1c926f2cbaf2ddebf872f0585b5d8da220be2
Akash kumar <158338367+akash-mehta47@users.noreply.github.com>
2026-07-04 18:23:01 +0530

    Time: 76 ms (40.22%), Space: 164.9 MB (16.33%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
-
-    void longestSubString(int left, int right, int& maxLength, int& start, string s){
+    void longestCheck(int left,int right, int &maxLength,int &start,string s){
         while(left>=0 && right<s.size() && s[left]==s[right]){
-            int currentLength= right-left+1;
-            if(currentLength>maxLength){
+            int length=right-left+1;
+            if(length>maxLength){
                 start=left;
-                maxLength=currentLength;
+                maxLength=length;
             }
             left--;
             right++;
@@ -14,12 +13,12 @@ public:
     }
 
     string longestPalindrome(string s) {
-        int size=1;
         int start=0;
+        int maxLength=1;
         for(int i=0;i<s.size();i++){
-            longestSubString(i,i,size,start,s);
-            longestSubString(i,i+1,size,start,s);
+            longestCheck(i,i,maxLength,start,s);
+            longestCheck(i,i+1,maxLength,start,s);
         }
-        return s.substr(start,size);
+        return s.substr(start,maxLength);
     }
 };
